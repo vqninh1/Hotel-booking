71f2c5b6ec84d652363cbe614c293123e27502f3
QuangNinh811 <vqninh1@gmail.com>
2021-10-27 16:50:20 +0700

    ninh-up-2

--- index.php
+++ index.php
@@ -41,9 +41,9 @@
 	          <li class="nav-item active"><a href="index.html" class="nav-link">Trang Chủ</a></li>
 	          <li class="nav-item"><a href="rooms.html" class="nav-link">Phòng</a></li>
 	          <li class="nav-item"><a href="restaurant.html" class="nav-link">Nhà Hàng</a></li>
-	          <li class="nav-item"><a href="about.html" class="nav-link">Giới thiệu</a></li>
+	          <li class="nav-item"><a href="about.html" class="nav-link">Giới Thiệu</a></li>
 	          <li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li>
-	          <li class="nav-item"><a href="contact.html" class="nav-link">Liên hệ</a></li>
+	          <li class="nav-item"><a href="contact.html" class="nav-link">Liên Hệ</a></li>
 	        </ul>
 	      </div>
 	    </div>
@@ -468,120 +468,9 @@
 			</div>
 		</section>
 		
-		<section class="ftco-section ftco-menu" style="background-image: url(images/restaurant-pattern.jpg);">
-			<div class="container">
-				<div class="row justify-content-center mb-5 pb-3">
-          <div class="col-md-7 heading-section text-center ftco-animate">
-          	<span class="subheading">Nhà Hàng</span>
-            <h2>Nartus Vincere</h2>
-          </div>
-        </div>
-				<div class="row">
-        	<div class="col-md-6">
-        		<div class="pricing-entry d-flex ftco-animate">
-        			<div class="img order-md-last" style="background-image: url(images/menu-1.jpg);"></div>
-        			<div class="desc pr-3 text-md-right">
-	        			<div class="d-md-flex text align-items-center">
-	        				<h3 class="order-md-last heading-left"><span>Grilled Crab with Onion</span></h3>
-	        				<span class="price price-left">$20.00</span>
-	        			</div>
-	        			<div class="d-block">
-	        				<p>A small river named Duden flows by their place and supplies</p>
-	        			</div>
-        			</div>
-        		</div>
-        		<div class="pricing-entry d-flex ftco-animate">
-        			<div class="img order-md-last" style="background-image: url(images/menu-2.jpg);"></div>
-        			<div class="desc pr-3 text-md-right">
-	        			<div class="d-md-flex text align-items-center">
-	        				<h3 class="order-md-last heading-left"><span>Grilled Beef with potatoes</span></h3>
-	        				<span class="price price-left">$29.00</span>
-	        			</div>
-	        			<div class="d-block">
-	        				<p>A small river named Duden flows by their place and supplies</p>
-	        			</div>
-	        		</div>
-        		</div>
-        		<div class="pricing-entry d-flex ftco-animate">
-        			<div class="img order-md-last" style="background-image: url(images/menu-3.jpg);"></div>
-        			<div class="desc pr-3 text-md-right">
-	        			<div class="d-md-flex text align-items-center">
-	        				<h3 class="order-md-last heading-left"><span>Grilled Beef with potatoes</span></h3>
-	        				<span class="price price-left">$20.00</span>
-	        			</div>
-	        			<div class="d-block">
-	        				<p>A small river named Duden flows by their place and supplies</p>
-	        			</div>
-	        		</div>
-        		</div>
-        		<div class="pricing-entry d-flex ftco-animate">
-        			<div class="img order-md-last" style="background-image: url(images/menu-4.jpg);"></div>
-        			<div class="desc pr-3 text-md-right">
-	        			<div class="d-md-flex text align-items-center">
-	        				<h3 class="order-md-last heading-left"><span>Grilled Beef with potatoes</span></h3>
-	        				<span class="price price-left">$20.00</span>
-	        			</div>
-	        			<div class="d-block">
-	        				<p>A small river named Duden flows by their place and supplies</p>
-	        			</div>
-	        		</div>
-        		</div>
-        	</div>
-
-        	<div class="col-md-6">
-        		<div class="pricing-entry d-flex ftco-animate">
-        			<div class="img" style="background-image: url(images/menu-5.jpg);"></div>
-        			<div class="desc pl-3">
-	        			<div class="d-md-flex text align-items-center">
-	        				<h3><span>Grilled Beef with potatoes</span></h3>
-	        				<span class="price">$49.91</span>
-	        			</div>
-	        			<div class="d-block">
-	        				<p>A small river named Duden flows by their place and supplies</p>
-	        			</div>
-	        		</div>
-        		</div>
-        		<div class="pricing-entry d-flex ftco-animate">
-        			<div class="img" style="background-image: url(images/menu-6.jpg);"></div>
-        			<div class="desc pl-3">
-	        			<div class="d-md-flex text align-items-center">
-	        				<h3><span>Ultimate Overload</span></h3>
-	        				<span class="price">$20.00</span>
-	        			</div>
-	        			<div class="d-block">
-	        				<p>A small river named Duden flows by their place and supplies</p>
-	        			</div>
-	        		</div>
-        		</div>
-        		<div class="pricing-entry d-flex ftco-animate">
-        			<div class="img" style="background-image: url(images/menu-7.jpg);"></div>
-        			<div class="desc pl-3">
-	        			<div class="d-md-flex text align-items-center">
-	        				<h3><span>Grilled Beef with potatoes</span></h3>
-	        				<span class="price">$20.00</span>
-	        			</div>
-	        			<div class="d-block">
-	        				<p>A small river named Duden flows by their place and supplies</p>
-	        			</div>
-	        		</div>
-        		</div>
-        		<div class="pricing-entry d-flex ftco-animate">
-        			<div class="img" style="background-image: url(images/menu-8.jpg);"></div>
-        			<div class="desc pl-3">
-	        			<div class="d-md-flex text align-items-center">
-	        				<h3><span>Ham &amp; Pineapple</span></h3>
-	        				<span class="price">$20.00</span>
-	        			</div>
-	        			<div class="d-block">
-	        				<p>A small river named Duden flows by their place and supplies</p>
-	        			</div>
-	        		</div>
-        		</div>
-        	</div>
-        </div>
-			</div>
-		</section>
-
+		<?php
+			include('./restaurant-detail.php')
+		?>
 
     <section class="ftco-section">
       <div class="container">
@@ -689,65 +578,10 @@
       </div>
     </section>
 
-    <footer class="ftco-footer ftco-bg-dark ftco-section">
-      <div class="container">
-        <div class="row mb-5">
-          <div class="col-md">
-            <div class="ftco-footer-widget mb-4">
-              <h2 class="ftco-heading-2">Hilton</h2>
-              <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-              <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
-                <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
-                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
-                <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
-              </ul>
-            </div>
-          </div>
-          <div class="col-md">
-            <div class="ftco-footer-widget mb-4 ml-md-5">
-              <h2 class="ftco-heading-2">Useful Links</h2>
-              <ul class="list-unstyled">
-                <li><a href="#" class="py-2 d-block">Blog</a></li>
-                <li><a href="#" class="py-2 d-block">Rooms</a></li>
-                <li><a href="#" class="py-2 d-block">Amenities</a></li>
-                <li><a href="#" class="py-2 d-block">Gift Card</a></li>
-              </ul>
-            </div>
-          </div>
-          <div class="col-md">
-             <div class="ftco-footer-widget mb-4">
-              <h2 class="ftco-heading-2">Privacy</h2>
-              <ul class="list-unstyled">
-                <li><a href="#" class="py-2 d-block">Career</a></li>
-                <li><a href="#" class="py-2 d-block">About Us</a></li>
-                <li><a href="#" class="py-2 d-block">Contact Us</a></li>
-                <li><a href="#" class="py-2 d-block">Services</a></li>
-              </ul>
-            </div>
-          </div>
-          <div class="col-md">
-            <div class="ftco-footer-widget mb-4">
-            	<h2 class="ftco-heading-2">Have a Questions?</h2>
-            	<div class="block-23 mb-3">
-	              <ul>
-	                <li><span class="icon icon-map-marker"></span><span class="text">1 Le Thanh Tong ,Hoan Kiem Dist, Ha Noi 10000, Viet Nam</span></li>
-	                <li><a href="#"><span class="icon icon-phone"></span><span class="text">024 3933 0500</span></a></li>
-	                <li><a href="#"><span class="icon icon-envelope"></span><span class="text">hanoi.opera@hilton.com</span></a></li>
-	              </ul>
-	            </div>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-12 text-center">
+    <?php
+		include('./footer.php')
 
-            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart color-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>.Downloaded from <a href="https://themeslab.org/" target="_blank">Themeslab</a>
-  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
-          </div>
-        </div>
-      </div>
-    </footer>
+	?>
     
   
 
