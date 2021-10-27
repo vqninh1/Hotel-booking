82e903d3861a6ec340c4ae5dfd73858705b52353
Ducanh2404 <1951060533@e.tlu.edu.vn>
2021-10-27 16:29:40 +0700

    ducanh-1

---
+++ footer.php
@@ -0,0 +1,49 @@
+<footer class="ftco-footer ftco-bg-dark ftco-section">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2">Hilton</h2>
+              <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+              <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
+                <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4 ml-md-5">
+              <h2 class="ftco-heading-2">Đường Dẫn Hữu Ích</h2>
+              <ul class="list-unstyled">
+                <li><a href="#" class="py-2 d-block">Blog</a></li>
+                <li><a href="#" class="py-2 d-block">Phòng</a></li>
+                <li><a href="#" class="py-2 d-block">Quà Tặng</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md">
+             <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2">Chính Sách</h2>
+              <ul class="list-unstyled">
+                <li><a href="#" class="py-2 d-block">Về Chúng Tôi</a></li>
+                <li><a href="#" class="py-2 d-block">Liên Hệ</a></li>
+                <li><a href="#" class="py-2 d-block">Dịch Vụ</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4">
+            	<h2 class="ftco-heading-2">Địa chỉ liên hệ</h2>
+            	<div class="block-23 mb-3">
+	              <ul>
+	                <li><span class="icon icon-map-marker"></span><span class="text">1 Le Thanh Tong ,Hoan Kiem Dist, Ha Noi 10000, Viet Nam</span></li>
+	                <li><a href="#"><span class="icon icon-phone"></span><span class="text">024 3933 0500</span></a></li>
+	                <li><a href="#"><span class="icon icon-envelope"></span><span class="text">hanoi.opera@hilton.com</span></a></li>
+	              </ul>
+	            </div>
+            </div>
+          </div>
+        </div>
+	</div>
+    </footer>
