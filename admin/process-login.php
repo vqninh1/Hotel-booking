b6bc4de602170f9b721be501a7c54abf180c765c
tuanr10 <tuanmanucian2001@gmail.com>
2021-10-30 17:53:27 +0700

    tuan-fix-process-login-index

---
+++ admin/process-login.php
@@ -0,0 +1,22 @@
+<?php
+    session_start();
+    require('config/db.php');
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $sql_1 = "SELECT * from db_users where name_user = '$username'";
+    $result_1 = mysqli_query($conn, $sql_1);
+
+    if (mysqli_num_rows($result_1)>0){
+       $row=mysqli_fetch_assoc($result_1);
+       $pass_saved = $row['password_user'];
+       if(password_verify($password, $pass_saved)){
+           $_SESSION['login_ok']=$username;
+           header("Location:index.php");
+       }else{
+        $response = 'failed_pass';
+       }
+    }else{
+        $response = 'failed_user';
+    }
+?>
