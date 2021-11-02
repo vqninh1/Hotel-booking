5e152f2767a372518c6868fce2daa3dd88ca0b7b
Ducanh2404 <1951060533@e.tlu.edu.vn>
2021-11-02 16:05:47 +0700

    fix

--- index.php
+++ index.php
@@ -138,7 +138,6 @@
 							<span class="flaticon-reception-bell"></span>
 						</div>
 					</div>
-<<<<<<< HEAD
 					<div class="media-body">
 						<a href="booking-table.php">
 							<h3 class="heading mb-3">Đặt Bàn</h3>
@@ -175,34 +174,6 @@
 					<div class="d-flex justify-content-center">
 						<div class="icon d-flex align-items-center justify-content-center">
 							<span class="ion-ios-bed"></span>
-=======
-					<div class="col-lg-6">
-						<div class="room-wrap d-md-flex">
-							<a href="#" class="img order-md-last" style="background-image: url(images/room-2.jpg);"></a>
-							<div class="half right-arrow d-flex align-items-center">
-								<div class="text p-4 p-xl-5 text-center">
-									<p class="star mb-0"><span class="ion-ios-star"></span><span class="ion-ios-star"></span><span class="ion-ios-star"></span><span class="ion-ios-star"></span><span class="ion-ios-star"></span></p>
-									<p class="mb-0"><span class="price mr-1">10.000.000VNĐ</span></p>
-									<span class="per">một đêm</span>
-									<h3 class="mb-3"><a href="rooms-classic.php">Phòng Cổ Điển</a></h3>
-									<p class="pt-1"><a href="rooms-classic.php" class="btn-custom px-3 py-2">Xem thông tin phòng <span class="icon-long-arrow-right"></span></a></p>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-6">
-						<div class="room-wrap d-md-flex">
-							<a href="#" class="img order-md-last" style="background-image: url(images/room-3.jpg);"></a>
-							<div class="half right-arrow d-flex align-items-center">
-								<div class="text p-4 p-xl-5 text-center">
-									<p class="star mb-0"><span class="ion-ios-star"></span><span class="ion-ios-star"></span><span class="ion-ios-star"></span><span class="ion-ios-star"></span><span class="ion-ios-star"></span></p>
-									<p class="mb-0"><span class="price mr-1">15.000.000VNĐ</span></p>
-									<span class="per">một đêm</span>
-									<h3 class="mb-3"><a href="rooms-family.php">Phòng Gia Đình</a></h3>
-									<p class="pt-1"><a href="rooms-family.php" class="btn-custom px-3 py-2">Xem thông tin phòng <span class="icon-long-arrow-right"></span></a></p>
-								</div>
-							</div>
->>>>>>> ducanh
 						</div>
 					</div>
 					<div class="media-body">
@@ -214,7 +185,6 @@
 	</div>
 </section>
 
-<<<<<<< HEAD
 <section class="ftco-section bg-light ftco-room">
 	<div class="container-fluid px-0">
 		<div class="row no-gutters justify-content-center mb-5 pb-3">
@@ -244,34 +214,6 @@
 							<span class="per">một đêm</span>
 							<h3 class="mb-3"><a href="rooms.html">Phòng Cổ Điển</a></h3>
 							<p class="pt-1"><a href="./rooms-single.html" class="btn-custom px-3 py-2">Xem thông tin phòng <span class="icon-long-arrow-right"></span></a></p>
-=======
-					<div class="col-lg-6">
-						<div class="room-wrap d-md-flex">
-							<a href="#" class="img" style="background-image: url(images/room-4.jpg);"></a>
-							<div class="half left-arrow d-flex align-items-center">
-								<div class="text p-4 p-xl-5 text-center">
-									<p class="star mb-0"><span class="ion-ios-star"></span><span class="ion-ios-star"></span><span class="ion-ios-star"></span><span class="ion-ios-star"></span><span class="ion-ios-star"></span></p>
-									<p class="mb-0"><span class="price mr-1">17.000.000VNĐ</span></p>
-									<span class="per">một đêm</span>
-									<h3 class="mb-3"><a href="rooms-deluxe.php">Phòng Deluxe</a></h3>
-									<p class="pt-1"><a href="rooms-deluxe.php" class="btn-custom px-3 py-2">Xem thông tin phòng <span class="icon-long-arrow-right"></span></a></p>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-6">
-						<div class="room-wrap d-md-flex">
-							<a href="#" class="img" style="background-image: url(images/room-6.jpg);"></a>
-							<div class="half left-arrow d-flex align-items-center">
-								<div class="text p-4 p-xl-5 text-center">
-									<p class="star mb-0"><span class="ion-ios-star"></span><span class="ion-ios-star"></span><span class="ion-ios-star"></span><span class="ion-ios-star"></span><span class="ion-ios-star"></span></p>
-									<p class="mb-0"><span class="price mr-1">25.000.000VNĐ</span></p>
-									<span class="per">một đêm</span>
-									<h3 class="mb-3"><a href="rooms-suite.php">Phòng Thượng Hạng</a></h3>
-									<p class="pt-1"><a href="rooms-suite.php" class="btn-custom px-3 py-2">Xem thông tin phòng <span class="icon-long-arrow-right"></span></a></p>
-								</div>
-							</div>
->>>>>>> ducanh
 						</div>
 					</div>
 				</div>
