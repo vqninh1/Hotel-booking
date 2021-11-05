5ca7e19a88693dbe7b1a97463f17b8e31b80d475
tuanr10 <tuanmanucian2001@gmail.com>
2021-11-06 02:22:26 +0700

    update-index

--- index.php
+++ index.php
@@ -148,7 +148,7 @@ include('header.php') ?>
     						<div class="text p-4 p-xl-5 text-center">
     							<p class="star mb-0"><span class="ion-ios-star"></span><span class="ion-ios-star"></span><span class="ion-ios-star"></span><span class="ion-ios-star"></span><span class="ion-ios-star"></span></p>
     							<p class="mb-0"><span class="price mr-1"><?php echo $price_rm?></span></p>
-								<span class="per">một đêm</span>
+								<span class="per">một ngày</span>
 	    						<h3 class="mb-3"><a href="rooms-details.php?id_rm=<?php echo $id_rm; ?>"><?php echo $name_rm?></a></h3>
 	    						<p class="pt-1"><a href="rooms-details.php?id_rm=<?php echo $id_rm; ?>" class="btn-custom px-3 py-2">Xem Chi Tiết Phòng <span class="icon-long-arrow-right"></span></a></p>
     						</div>
